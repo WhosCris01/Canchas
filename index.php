dbfe3b7b01226230b1ba3a65274e0882c29f7307
WhosCris <cris.ibarra211@gmail.com>
2023-12-02 11:58:21 -0300

    Se carga el repositorio a la nube

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+    <title>Inicio de Sesion</title>
+</head>
+<body>
+    <form method="post">
+        <tr>
+            <td>Usuario</td>
+            <td><input type="text" name="userBox" value="" size="10"></td>
+            </tr>
+            <td>Contraseña</td>
+            <td><input type="password" name="passwordBox" value="" size="10">
+            </td>
+        </tr>
+    </form>
+    
+</body>
+</html>
