2883c73738255798676801724fb978049484ef25
WhosCris01 <cristian.ibarramu@correoaiep.cl>
2023-12-12 19:36:38 -0300

    Se modifica el index para el diseño principal

--- index.php
+++ index.php
@@ -4,77 +4,25 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <!-- INCORPORAR LA HORA A UN FORMULARIO -->
-	<script language="javascript">
-	var RelojID24 = null
-	var RelojEjecutandose24 = false
-	function DetenerReloj24 (){
-		if(RelojEjecutandose24)
-		clearTimeout(RelojID24)
-		RelojEjecutandose24 = false}
-	function MostrarHora24 () {
-		var ahora = new Date()
-		var horas = ahora.getHours()
-		var minutos = ahora.getMinutes()
-		var segundos = ahora.getSeconds()
-		var ValorHora
-	//establece las horas
-	if (horas < 10)
-		ValorHora = "0" + horas
-	else
-		ValorHora = "" + horas
-	//establece los minutos
-	if (minutos < 10)
-		ValorHora += ":0" + minutos
-	else
-		ValorHora += ":" + minutos
-	//establece los segundos
-	if (segundos < 10)
-		ValorHora += ":0" + segundos
-	else
-		ValorHora += ":" + segundos
-		document.reloj24.txtDigitos.value = ValorHora
-	//si se desea tener el reloj en la barra de estado, reemplazar la anterior por esta
-	//window.status = ValorHora
-		RelojID24 = setTimeout("MostrarHora24()",1000)
-		RelojEjecutandose24 = true
-	}
-	function IniciarReloj24 () {
-		DetenerReloj24()
-		MostrarHora24()
-	}
-	</script>
+    <link rel="stylesheet" href="css/index.css">
+    <title>Iniciar sesion</title>
     </head>
 
-    <body onload="IniciarReloj24()">
-    <form name="reloj24"> <!-- Este form es otro aparte del que usamos para nuestro formulario -->
-        <input type="text" size="6" name="txtDigitos" style="background-color:E7DE4D;
-		border-color:transparent; text-align:right" value="" disabled>
+    <body>
+            <div class="container">
 
-		<!-- COLOCAR LA FECHA ACTUAL AL FORMULARIO -->
-	<?php date_default_timezone_set('America/Santiago');
-            $vaFecha=date('d-M-Y');
-        ?>
-        <input type="text" name="caja_fecha" size="10" style="background-color:E7DE4D;
-		border-color:transparent; text-align:right" value="<?php echo $vaFecha; ?>" disabled>
-    </form>
-	
-</body>
-
-
-    <body bgcolor="E7DE4D">
-    <center>
         <form method="post">
         <?php error_reporting (0); ?>
-        <h1><FONT COLOR="white">INICIAR SESIÓN </FONT></h1>
-    <table><br><br><br>
+        <h1>INICIAR SESIÓN</h1>
     
         <tr>
-            <td><b>Usuario : <b></td><td><input type="textRut" name="txtRut" value="" placeholder="ingrese su rut"></td>
+            <td><b>Usuario :
+            </td><td><input type="textRut" name="txtRut" value="" placeholder="ingrese su rut"></td>
         </tr>
-        <tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr></tr>
+        <br>
         <tr>
-            <td><b>Clave : <b></td><td> <input type="password" name="txtContra" value="" maxlenght="15" placeholder="ingrese su contraseña"></td>
+            <td><b>Clave : <b></td>
+            <td> <input type="password" name="txtContra" value="" maxlenght="15" placeholder="ingrese su contraseña"></td>
         </tr>
        
 
